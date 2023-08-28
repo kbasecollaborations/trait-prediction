5539174b23ddcbb5d67f5ed00aa04caf4adab87f
Dileep Kishore <k.dileep1994@gmail.com>
2023-08-28 16:27:26 -0400

    Add: Functions to read features
    1. `read_rast_features`
    2. `read_interpro_featuers`
    3. Also fix issues with poetry env

---
+++ trait_prediction/utils/read_features.py
@@ -0,0 +1,58 @@
+"""Module that contains functions for reading data from files."""
+
+import pathlib
+import pandas as pd
+
+
+def read_rast_features(
+    rast_feature_file: pathlib.Path,
+) -> tuple[pd.DataFrame, dict[str, str]]:
+    """
+    Reads the rast features from the given file and returns them as a pandas DataFrame.
+
+    Parameters
+    ---------
+    rast_feature_file : pathlib.Path
+        Path to the file containing the rast features.
+
+    Returns
+    ------
+    pd.DataFrame
+        Pandas DataFrame containing the rast features.
+    dict[str, str]
+        Dictionary mapping the rast subsystem ontology (SSO) to the full name.
+    """
+    rast_df = pd.read_csv(rast_feature_file, sep="\t", index_col=0)
+    rast_annotations = list(rast_df.columns)
+    rast_sso_dict = {x.split("__")[0]: x.split("__", 1)[-1] for x in rast_annotations}
+    rast_sso = list(rast_sso_dict.keys())
+    rast_df.columns = rast_sso
+    return rast_df, rast_sso_dict
+
+
+def read_interpro_features(
+    interpro_feature_file: pathlib.Path,
+) -> tuple[pd.DataFrame, dict[str, str]]:
+    """
+    Reads the interpro features from the given file and returns them as a pandas DataFrame.
+
+    Parameters
+    ---------
+    interpro_feature_file : pathlib.Path
+        Path to the file containing the interpro features.
+
+    Returns
+    ------
+    pd.DataFrame
+        Pandas DataFrame containing the interpro features.
+    dict[str, str]
+        Dictionary mapping the interpro ontology (IPR) to the full name.
+    """
+    interpro_df = pd.read_csv(interpro_feature_file, sep="\t", index_col=0)
+    interpro_annotations = list(interpro_df.columns)
+    interpro_sso_dict = {
+        x.split("__")[0]: x.split("__", 1)[-1] for x in interpro_annotations
+    }
+    interpro_sso = list(interpro_sso_dict.keys())
+    interpro_df.columns = interpro_sso
+    return interpro_df, interpro_sso_dict
