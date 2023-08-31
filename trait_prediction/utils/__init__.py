09413ad4906bb5d6f984f0133b6dfe74800200a0
Dileep Kishore <k.dileep1994@gmail.com>
2023-08-30 22:30:46 -0400

    Fix: Remove deleted file import from utils.__init__

--- trait_prediction/utils/__init__.py
+++ trait_prediction/utils/__init__.py
@@ -1,2 +1,1 @@
-from .read_data import read_phenotype_data
-from .read_features import read_rast_features, read_interpro_features
+from .read_features import read_interpro_features, read_rast_features
