0329410549c1df7fa3361c8dfcc89ce1b91d6caf
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-06 18:40:00 -0500

    Change: Change the order of execution of phenotype prediction runs

--- batch_predict_traits.sh
+++ batch_predict_traits.sh
@@ -4,39 +4,6 @@
 
 ## Commands
 
-# All features
-python -W ignore run_predict_traits.py data/outputs/biolog \
-    --cross_validate \
-    --random_state 42 \
-    --n_cpus 4
-# Reduction: NMF=200
-python -W ignore run_predict_traits.py data/outputs/biolog_nmf_200 \
-    --reduction_func "NMF" \
-    --n_features 200 \
-    --cross_validate \
-    --random_state 42 \
-    --n_cpus 16
-# Reduction: PCA=200
-python -W ignore run_predict_traits.py data/outputs/biolog_pca_200 \
-    --reduction_func "PCA" \
-    --n_features 200 \
-    --cross_validate \
-    --random_state 42 \
-    --n_cpus 16
-# Reduction: NMF=100
-python -W ignore run_predict_traits.py data/outputs/biolog_nmf_100 \
-    --reduction_func "NMF" \
-    --n_features 100 \
-    --cross_validate \
-    --random_state 42 \
-    --n_cpus 16
-# Reduction: PCA=100
-python -W ignore run_predict_traits.py data/outputs/biolog_pca_100 \
-    --reduction_func "PCA" \
-    --n_features 100 \
-    --cross_validate \
-    --random_state 42 \
-    --n_cpus 16
 # Scoring: chi2=1000
 python -W ignore run_predict_traits.py data/outputs/biolog_chi2_1000 \
     --score_func "chi2" \
@@ -58,6 +25,22 @@ python -W ignore run_predict_traits.py data/outputs/biolog_f_1000 \
     --cross_validate \
     --random_state 42 \
     --n_cpus 16
+
+# Reduction: NMF=200
+python -W ignore run_predict_traits.py data/outputs/biolog_nmf_200 \
+    --reduction_func "NMF" \
+    --n_features 200 \
+    --cross_validate \
+    --random_state 42 \
+    --n_cpus 16
+# Reduction: PCA=200
+python -W ignore run_predict_traits.py data/outputs/biolog_pca_200 \
+    --reduction_func "PCA" \
+    --n_features 200 \
+    --cross_validate \
+    --random_state 42 \
+    --n_cpus 16
+
 # Scoring: chi2=500
 python -W ignore run_predict_traits.py data/outputs/biolog_chi2_500 \
     --score_func "chi2" \
@@ -76,6 +59,27 @@ python -W ignore run_predict_traits.py data/outputs/biolog_mutual_info_500 \
 python -W ignore run_predict_traits.py data/outputs/biolog_f_500 \
     --score_func "f_classif" \
     --n_features 500 \
+    --cross_validate \
+    --random_state 42 \
+    --n_cpus 16
+
+# Reduction: NMF=100
+python -W ignore run_predict_traits.py data/outputs/biolog_nmf_100 \
+    --reduction_func "NMF" \
+    --n_features 100 \
+    --cross_validate \
+    --random_state 42 \
+    --n_cpus 16
+# Reduction: PCA=100
+python -W ignore run_predict_traits.py data/outputs/biolog_pca_100 \
+    --reduction_func "PCA" \
+    --n_features 100 \
+    --cross_validate \
+    --random_state 42 \
+    --n_cpus 16
+
+# All features
+python -W ignore run_predict_traits.py data/outputs/biolog \
     --cross_validate \
     --random_state 42 \
     --n_cpus 16
