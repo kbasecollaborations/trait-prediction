eee93a1346899c2c202a4334d9038c857d637b8f
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-06 14:04:32 -0500

    Add: Script to copy data to cades through scp

---
+++ cades_scp.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+scp -r trait_prediction notebooks tests cloud-cades:/datax/dkishore/trait-prediction/
+scp -r *.py *.sh *.lock *.nix *.toml *.md LICENSE cloud-cades:/datax/dkishore/trait-prediction/
+scp -r data/processed cloud-cades:/datax/dkishore/trait-prediction/data/
