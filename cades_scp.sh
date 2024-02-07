7375670cf8aaa75562f444dc06acf6dfd603acf0
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-07 00:41:45 -0500

    Change: Do not copy over data folder

--- cades_scp.sh
+++ cades_scp.sh
@@ -2,4 +2,4 @@
 
 scp -r trait_prediction notebooks tests cloud-cades:/datax/dkishore/trait-prediction/
 scp -r *.py *.sh *.lock *.nix *.toml *.md LICENSE cloud-cades:/datax/dkishore/trait-prediction/
-scp -r data/processed cloud-cades:/datax/dkishore/trait-prediction/data/
+# scp -r data/processed cloud-cades:/datax/dkishore/trait-prediction/data/
