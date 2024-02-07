6803c69143f24704d8176cdb794fb546eea77270
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-07 00:36:47 -0500

    Add: Corr calc now supports multiple methods
    1. numpy: This is the default method and uses numpy matrix multiplication
       to calculate correlations
    2. numba_parallel: This method uses numba to parallelize the correlation
       calculation. This method is slower than numpy but does not store the
       correlation matrix in memory
    3. numba: This method uses numba without parallelization and is much slower
       than the other methods. This method is not recommended for large datasets

--- trait_prediction/feature_selection/reduction.py
+++ trait_prediction/feature_selection/reduction.py
@@ -44,9 +44,11 @@ def remove_features_with_low_variance(
 
 
 @jit(nopython=True, parallel=True)
-def _find_corr_cols_todrop(matrix: np.ndarray, threshold=0.95) -> list[list[int]]:
+def _find_corr_cols_numba_parallel(
+    matrix: np.ndarray, threshold=0.95
+) -> list[list[int]]:
     """
-    Find columns to drop based on correlation threshold
+    Find columns to drop based on correlation threshold (parallelism enabled)
 
     Parameters
     ----------
@@ -76,8 +78,93 @@ def _find_corr_cols_todrop(matrix: np.ndarray, threshold=0.95) -> list[list[int]
     return corr_cols
 
 
+@jit(nopython=True)
+def _find_corr_cols_numba(matrix: np.ndarray, threshold=0.95) -> list[list[int]]:
+    """
+    Find columns to drop based on correlation threshold (parallelism disabled)
+
+    Parameters
+    ----------
+    matrix : np.ndarray
+        2-dimensional matrix. M (data points) x N (features)
+    threshold : float, optional
+        Correlation threshold, by default 0.95
+
+    Returns
+    -------
+    corr_cols
+        List of lists containing correlated columns for each columns
+    """
+    m = matrix.shape[0]
+    n = matrix.shape[1]
+    corr_cols = [typed.List.empty_list(types.int64) for _ in range(n)]
+    for i in range(n):
+        to_drop = typed.List.empty_list(types.int64)
+        for j in range(i + 1, n):
+            xi, xj = matrix[:, i], matrix[:, j]
+            mean_i, mean_j = np.mean(xi), np.mean(xj)
+            std_i, std_j = np.std(xi), np.std(xj)
+            corr = np.sum((xi - mean_i) * (xj - mean_j)) / (m * std_i * std_j)  # type: ignore
+            if corr >= threshold:
+                to_drop.append(j)
+        corr_cols[i] = to_drop
+    return corr_cols
+
+
+def _pearson_correlation_coefficient(X: np.ndarray) -> np.ndarray:
+    """
+    Calculate the Pearson correlation coefficient of matrix X.
+
+    Parameters
+    ----------
+    X : numpy array (MxN)
+        The input matrix with M rows (number of features) and N columns (number of data points).
+
+    Returns
+    -------
+    pcc : numpy array (MxM)
+        The Pearson correlation coefficient matrix, where each element (i, j) represents the correlation
+        between feature i and feature j in the input matrix X.
+    """
+    M, N = X.shape[0], X.shape[1]  # number of features, number of data points
+    X_mean = np.mean(X, axis=1).reshape(M, 1)
+    X_std = np.std(X, axis=1).reshape(M, 1)
+    X_tilde = (X - X_mean) / X_std
+    pcc = X_tilde @ X_tilde.T / N
+    np.fill_diagonal(pcc, 1, wrap=False)
+    return pcc
+
+
+@jit(nopython=True)
+def _find_corr_cols_numpy(corr_matrix: np.ndarray, threshold: float) -> list[list[int]]:
+    """
+    Find columns to drop based on correlation matrix and threshold.
+
+    Parameters
+    ----------
+    corr_matrix : numpy.ndarray
+        The M rows and M columns (number of features).
+    threshold : float
+        The correlation threshold.
+
+    Returns
+    -------
+    corr_cols
+        List of lists containing correlated columns for each columns
+    """
+    n_cols = corr_matrix.shape[1]
+    corr_cols = [typed.List.empty_list(types.int64) for _ in range(n_cols)]
+    for i in range(n_cols):
+        to_drop = typed.List.empty_list(types.int64)
+        for j in range(i + 1, n_cols):
+            if corr_matrix[i, j] >= threshold:
+                to_drop.append(j)
+        corr_cols[i] = to_drop
+    return corr_cols
+
+
 def remove_features_with_high_correlation(
-    feature_df: pd.DataFrame, threshold: float = 0.95
+    feature_df: pd.DataFrame, threshold: float = 0.95, method: str = "numpy"
 ) -> tuple[pd.DataFrame, dict[str, list[str]]]:
     """
     Removes features with high correlation from the given feature DataFrame.
@@ -89,6 +176,10 @@ def remove_features_with_high_correlation(
     threshold : float
         Threshold for the correlation of the features.
         Default value is 0.95
+    method : str, optional
+        Method used to calculate correlated features
+        Options available are 'numpy', 'numba_parallel' and 'numba'
+        Default value is 'numpy'
 
     Returns
     ------
@@ -98,7 +189,17 @@ def remove_features_with_high_correlation(
         Dictionary of the features with high correlation that were removed
     """
     # Get correlated columns
-    corr_cols = _find_corr_cols_todrop(feature_df.to_numpy(), threshold)
+    if method == "numba_parallel":
+        corr_cols = _find_corr_cols_numba_parallel(feature_df.to_numpy(), threshold)
+    elif method == "numba":
+        corr_cols = _find_corr_cols_numba(feature_df.to_numpy(), threshold)
+    elif method == "numpy":
+        corr_matrix = _pearson_correlation_coefficient(feature_df.to_numpy().T)
+        corr_cols = _find_corr_cols_numpy(corr_matrix, threshold)
+    else:
+        raise ValueError(
+            f"method {method} is not supported. Supported methods are numpy, numba_parallel, numba"
+        )
     # Find columns to drop
     cols_to_drop = set()
     corr_group_dict = dict()
