8cb3fb0d14134c28ab55d420a1e639d51628078a
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-11 17:54:37 -0400

    chore(flake): update flake

--- flake.nix
+++ flake.nix
@@ -7,74 +7,81 @@
     devenv.url = "github:cachix/devenv";
   };
   nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
-      packages = forEachSystem (system: {
-        devenv-up = self.devShells.${system}.default.config.procfileScript;
-      });
+      default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          {
+            # https://devenv.sh/reference/options/
+            packages = with pkgs; [
+              stdenv.cc.cc.lib # required by Jupyter
+              zlib
+              glibc
+              python311Packages.tkinter
+              # (python311.withPackages python-packages)
+            ];
 
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = devenv.lib.mkShell {
-            inherit inputs pkgs;
-            modules = [{
-              # https://devenv.sh/reference/options/
-              packages = with pkgs; [
-                stdenv.cc.cc.lib # required by Jupyter
-                zlib
-                glibc
-                python311Packages.tkinter
-                # (python311.withPackages python-packages)
-              ];
+            # https://devenv.sh/basics/
+            env = {GREET = "🛠️ Let's hack 💻";};
 
-              # https://devenv.sh/basics/
-              env = { GREET = "🛠️ Let's hack 💻"; };
+            # https://devenv.sh/scripts/
+            scripts.hello.exec = "echo $GREET";
 
-              # https://devenv.sh/scripts/
-              scripts.hello.exec = "echo $GREET";
+            enterShell = ''
+              hello
+            '';
 
-              enterShell = ''
-                hello
-              '';
-
-              # https://devenv.sh/languages/
-              languages.python = {
-                package = pkgs.python311;
+            # https://devenv.sh/languages/
+            languages.python = {
+              package = pkgs.python311;
+              enable = true;
+              poetry = {
                 enable = true;
-                poetry = {
-                  enable = true;
-                  activate.enable = true;
-                  install.enable = true;
-                  install.installRootPackage = true;
-                  install.allExtras = true;
-                };
+                activate.enable = true;
+                install.enable = true;
+                install.installRootPackage = true;
+                install.allExtras = true;
               };
+            };
 
-              # Make diffs fantastic
-              difftastic.enable = true;
+            # Make diffs fantastic
+            difftastic.enable = true;
 
-              # https://devenv.sh/pre-commit-hooks/
-              pre-commit.hooks = {
-                black.enable = true;
-                nixfmt.enable = true;
-                yamllint.enable = true;
-                editorconfig-checker.enable = true;
-              };
+            # https://devenv.sh/pre-commit-hooks/
+            pre-commit.hooks = {
+              black.enable = true;
+              alejandra.enable = true;
+              yamllint.enable = true;
+              editorconfig-checker.enable = true;
+            };
 
-              # Plugin configuration
-              pre-commit.settings = { yamllint.relaxed = true; };
+            # Plugin configuration
+            pre-commit.hooks.yamllint.settings.preset = "relaxed";
 
-              # https://devenv.sh/integrations/dotenv/
-              dotenv.enable = true;
-            }];
-          };
-        });
-    };
+            # https://devenv.sh/integrations/dotenv/
+            dotenv.enable = true;
+          }
+        ];
+      };
+    });
+  };
 }
