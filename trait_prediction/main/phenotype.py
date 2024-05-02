2e186135a0c202b7311ae4cacfbfe0f4ebf4f00e
Dileep Kishore <k.dileep1994@gmail.com>
2024-05-02 10:05:04 -0400

    feat(Phenotype): add read_data method

--- trait_prediction/main/phenotype.py
+++ trait_prediction/main/phenotype.py
@@ -34,22 +34,6 @@ class Phenotype:
         self.category = category
         self._phenotype_data = self._parse_phenotype_data(raw_phenotype_data)
 
-    def __repr__(self) -> str:
-        size = self._phenotype_data.shape[0]
-        return f"Phenotype (name={self.name}, category={self.category}, size={size})"
-
-    def __hash__(self) -> int:
-        unique_id = {
-            "name": self.name,
-            "category": self.category,
-        }
-        return hash(unique_id)
-
-    @property
-    def phenotype_data(self) -> pd.Series:
-        """Pandas Series containing the filtered phenotype data."""
-        return self._phenotype_data.copy(deep=True)
-
     def _parse_phenotype_data(self, raw_phenotype_data: pd.Series) -> pd.Series:
         """
         Parses the given raw phenotype data.
@@ -68,6 +52,50 @@ class Phenotype:
         return undup_raw_phenotype_data.loc[
             ~undup_raw_phenotype_data.index.duplicated(keep="first")
         ]
+
+    def __repr__(self) -> str:
+        size = self._phenotype_data.shape[0]
+        return f"Phenotype (name={self.name}, category={self.category}, size={size})"
+
+    def __hash__(self) -> int:
+        unique_id = {
+            "name": self.name,
+            "category": self.category,
+        }
+        return hash(unique_id)
+
+    @property
+    def phenotype_data(self) -> pd.Series:
+        """Pandas Series containing the filtered phenotype data."""
+        return self._phenotype_data.copy(deep=True)
+
+    @classmethod
+    def read_data(cls, file_path: str | pathlib.Path, category: str) -> "Phenotype":
+        """Read the phenotype data from the file.
+
+        Parameters
+        ----------
+        file_path : str | pathlib.Path
+            The file path to the phenotype data.
+        category : str
+            The category of the phenotype.
+
+        Returns
+        -------
+        "Phenotype"
+            The Phenotype object.
+        """
+        # NOTE: We use Int64 to handle NaN values
+        raw_phenotype_df = pd.read_csv(
+            file_path, sep="\t", index_col=0, dtype={"genomeID": str}
+        ).astype("Int64")
+        if raw_phenotype_df.index.name != "genomeID":
+            raise ValueError("The index of the Phenotype table must be 'genomeID'")
+        if raw_phenotype_df.shape[1] > 1:
+            raise ValueError("The Phenotype table can only contain one phenotype")
+        name = str(raw_phenotype_df.columns[0])
+        phenotype_data = raw_phenotype_df.loc[:, name]
+        return Phenotype(phenotype_data, name, category)
 
     def save(self, file_path: str | pathlib.Path) -> None:
         """
