bc68422ead45e96a062ffb97fd70a7ef8b3dbdef
Dileep Kishore <k.dileep1994@gmail.com>
2024-05-03 08:50:13 -0400

    feat(main): Add Index and Input classes to the main module scope

--- trait_prediction/main/__init__.py
+++ trait_prediction/main/__init__.py
@@ -1,2 +1,4 @@
-from .phenotype import Phenotype
+from .feature import Feature, FeatureIndex, FeatureInput
+from .feature_set import FeatureSet
+from .phenotype import Phenotype, PhenotypeIndex, PhenotypeInput
 from .phenotype_set import PhenotypeIndex, PhenotypeSet
