eff8aac61ebe24d442a9ea999ae965875e0b5e0d
Dileep Kishore <k.dileep1994@gmail.com>
2024-05-03 09:20:06 -0400

    feat(main): Add DataSet class to main module scope

--- trait_prediction/main/__init__.py
+++ trait_prediction/main/__init__.py
@@ -1,3 +1,4 @@
+from .dataset import DataSet
 from .feature import Feature, FeatureIndex, FeatureInput
 from .feature_set import FeatureSet
 from .phenotype import Phenotype, PhenotypeIndex, PhenotypeInput
