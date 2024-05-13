e05fd6c4a7949c3062acd8d2b6e5ccc1a13ae68f
Dileep Kishore <k.dileep1994@gmail.com>
2024-05-13 07:52:06 -0400

    feat(logger): remove config (file sink) from logger

--- trait_prediction/logging/logger.py
+++ trait_prediction/logging/logger.py
@@ -2,13 +2,4 @@
 
 from loguru import logger
 
-config = {
-    "handlers": [
-        # Add a logger that is multiprocess-safe
-        {"sink": "file.log", "enqueue": True},
-    ],
-    "extra": None,
-}
-logger.configure(**config)
-
 logger.disable("trait_prediction")
