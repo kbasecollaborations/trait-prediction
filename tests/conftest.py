4fa165be9984011cc41982469056d9cdef491bf3
dileep-kishore <k.dileep1994@gmail.com>
2024-05-14 11:08:53 -0400

    test(Feature): Add tests for Feature and FeatureSet classes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
+import random
 from pathlib import Path
 
 import pytest
 
-from trait_prediction.main import PhenotypeIndex, PhenotypeInput
+from trait_prediction.main import (
+    FeatureIndex,
+    FeatureInput,
+    FeatureSet,
+    PhenotypeIndex,
+    PhenotypeInput,
+    PhenotypeSet,
+)
 
 
 @pytest.fixture
@@ -48,3 +56,42 @@ def leaf_phenotype_pinputs(leaf_phenotype_folder):
         )
         pinputs.append(pinput)
     return pinputs
+
+
+@pytest.fixture
+def leaf_phenotype(leaf_phenotype_pinputs):
+    phenotype_set = PhenotypeSet.read_data(leaf_phenotype_pinputs)
+    return random.choice(list(phenotype_set.phenotypes))
+
+
+@pytest.fixture
+def leaf_phenotype_set(leaf_phenotype_pinputs):
+    return PhenotypeSet.read_data(leaf_phenotype_pinputs)
+
+
+@pytest.fixture
+def leaf_feature(leaf_feature_finputs):
+    feature_set = FeatureSet.read_data(leaf_feature_finputs)
+    return random.choice(list(feature_set.features))
+
+
+@pytest.fixture
+def leaf_feature_set(leaf_feature_finputs):
+    return FeatureSet.read_data(leaf_feature_finputs)
+
+
+@pytest.fixture
+@pytest.fixture
+def leaf_feature_finputs(leaf_feature_folder):
+    finputs = []
+    for feature_file in leaf_feature_folder.iterdir():
+        name = feature_file.stem
+        ftype = "binary"
+        dtype = "uint8"
+        findex = FeatureIndex(name=name, ftype=ftype, dtype=dtype)
+        index_format_func = lambda x: x
+        finput = FeatureInput(
+            path=feature_file, findex=findex, index_format_func=index_format_func
+        )
+        finputs.append(finput)
+    return finputs
---
+++ tests/test_feature.py
@@ -0,0 +1,80 @@
+import pytest
+
+from trait_prediction.main import (
+    Feature,
+    FeatureIndex,
+    FeatureInput,
+    FeatureSet,
+    PhenotypeIndex,
+    PhenotypeInput,
+    PhenotypeSet,
+)
+
+
+def test_feature_read_data(leaf_feature_folder):
+    for feature_file in leaf_feature_folder.iterdir():
+        name = feature_file.stem
+        ftype = "binary"
+        dtype = "uint8"
+        findex = FeatureIndex(name=name, ftype=ftype, dtype=dtype)
+        index_format_func = lambda x: x
+        finput = FeatureInput(
+            path=feature_file, findex=findex, index_format_func=index_format_func
+        )
+        feature = Feature.read_data(finput)
+        assert feature.feature_data is not None
+        assert feature.feature_data.shape[0] > 0
+        assert feature.feature_data.shape[1] > 0
+        assert feature.findex.name == feature_file.stem
+
+
+def test_featureset_read_data(leaf_feature_finputs):
+    feature_set = FeatureSet.read_data(leaf_feature_finputs)
+    assert len(feature_set) == len(leaf_feature_finputs)
+
+
+def tests_featureset_get_feature(leaf_feature_finputs):
+    feature_set = FeatureSet.read_data(leaf_feature_finputs)
+    findex = FeatureIndex(name="kofam_20", ftype="binary", dtype="uint8")
+    feature = feature_set.get_feature(findex)
+    assert feature.findex.name == "kofam_20"
+
+
+def test_feature_var(leaf_feature):
+    feature = leaf_feature
+    new_feature_data, removed_features = Feature.remove_features_with_low_variance(
+        feature.feature_data, threshold=0.1
+    )
+    assert len(removed_features) >= 1
+    assert new_feature_data.shape[1] == feature.feature_data.shape[1] - len(
+        removed_features
+    )
+
+
+@pytest.mark.parametrize("method", ["numpy", "numba", "numba_parallel"])
+def test_feature_correlation(method, leaf_feature):
+    feature = leaf_feature
+    new_feature_data, corr_dict = Feature.remove_features_with_high_correlation(
+        feature.feature_data, threshold=0.5, method=method
+    )
+    assert len(corr_dict) >= 1
+    assert new_feature_data.shape[1] < feature.feature_data.shape[1]
+
+
+# FIXME: Use dataset to test this
+@pytest.mark.parametrize("score_func", ["f_classif", "chi2", "mutual_info_classif"])
+def test_feature_kbest(score_func, leaf_phenotype, leaf_feature):
+    feature = leaf_feature
+    phenotype = leaf_phenotype
+    new_feature_data, low_score_features = Feature.feature_selection_kbest(
+        feature.feature_data,
+        phenotype.phenotype_data,
+        score_func=score_func,
+        n_features=5,
+    )
+    assert len(low_score_features) == 14
+    assert new_feature_data.shape[1] == 5
+
+
+def test_feature_dimreduction():
+    pass
