6a5bb1eaef72412a8484085476f6de231e6f56aa
Dileep Kishore <k.dileep1994@gmail.com>
2024-05-17 07:45:42 -0400

    feat(Pipeline): Add ConfigSet and ExperimentSet classes

--- trait_prediction/pipeline/__init__.py
+++ trait_prediction/pipeline/__init__.py
@@ -1,5 +1,5 @@
-from .config import Config
-from .experiment import Experiment
+from .config import Config, ConfigSet
+from .experiment import Experiment, ExperimentSet
 from .prediction_pipeline import PredictionPipeline
 
-__all__ = ["Config", "Experiment", "PredictionPipeline"]
+__all__ = ["Config", "ConfigSet", "Experiment", "ExperimentSet", "PredictionPipeline"]
