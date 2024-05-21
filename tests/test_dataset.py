1f9f8a87ed9faffcb44fbbbdfbe3c609d2fa630d
Dileep Kishore <k.dileep1994@gmail.com>
2024-05-21 08:08:07 -0400

    test: add fixtures for ch dataset and tests for DataSet.merge_data

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from trait_prediction.main import DataSet, FeatureIndex, PhenotypeIndex
 
 
@@ -30,3 +32,34 @@ def test_dataset_get_data(leaf_dataset):
     assert feature.feature_data is not None
     assert phenotype.phenotype_data.shape[0] == feature.feature_data.shape[0]
     assert (phenotype.phenotype_data.index == feature.feature_data.index).all()
+
+
+def test_dataset_merge(
+    leaf_phenotype_pinputs,
+    ch_phenotype_pinputs,
+    leaf_feature_finputs,
+    ch_feature_finputs,
+):
+    phenotype_groups = defaultdict(list)
+    feature_groups = defaultdict(list)
+    for pinput in leaf_phenotype_pinputs + ch_phenotype_pinputs:
+        phenotype_groups[pinput.pindex.name].append(pinput)
+    for finput in leaf_feature_finputs + ch_feature_finputs:
+        feature_groups[finput.findex.name].append(finput)
+    pinput_tuples = []
+    finput_tuples = []
+    for _, phenotype_group in phenotype_groups.items():
+        phenotype_group_list = tuple(phenotype_group)
+        if len(phenotype_group_list) > 1:
+            pinput_tuples.append(phenotype_group_list)
+    for _, feature_group in feature_groups.items():
+        feature_group_list = tuple(feature_group)
+        if len(feature_group_list) > 1:
+            finput_tuples.append(feature_group_list)
+    dataset = DataSet.merge_data(pinput_tuples, finput_tuples)
+    dataset_leaf = DataSet.read_data(leaf_phenotype_pinputs, leaf_feature_finputs)
+    phenotype_names = {p.pindex.name for p in dataset.phenotypes}
+    assert phenotype_names == {"histidine", "maltose", "galacturonic_acid"}
+    assert max([len(p.phenotype_data) for p in dataset.phenotypes]) > max(
+        [len(p.phenotype_data) for p in dataset_leaf.phenotypes]
+    )
